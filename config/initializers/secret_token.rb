bd50854699dbb5fd602f7345f445c37c138004c0
threekings <3thkings@gmail.com>
2011-05-11 12:40:43 +0800

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KyokushinApp::Application.config.secret_token = '0ec02a282c21db80225567b36f6012f5780c53d10306211b797fc9a04c3f0bbee3c805772bfe3d16f2f7936be9a1293edeee7c3aee63954fc4387d50ee209812'
